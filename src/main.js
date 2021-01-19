10f352072d970e6b39636446ee7307ad34e52d74
lotosv2010 <lotosv2010@163.com>
2021-01-19 23:02:21 +0800

    feat(commander): init commander

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('gt-cli')
+/* eslint-disable no-undef */
+const program = require('commander');
+const { version } = require('./utils/constants');
+
+// 版本
+program.version(`gt-cli @${version}`)
+  .usage(`<command> [option]`)
+
+// 解析用户执行命令传入的参数
+program.parse(process.argv);
