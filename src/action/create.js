be36786bf2fc6d68624824409ef24901b698aceb
lotosv2010 <lotosv2010@163.com>
2021-01-21 22:22:15 +0800

    feat(inquirer & ora): add inquirer & ora

--- src/action/create.js
+++ src/action/create.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+const ora = require('ora');
+const Inquirer = require('inquirer');
 
 // 获取仓库列表
 const fetchRepoList = async() => {
-  console.log(111)
   // 获取当前组织中的所有仓库信息,这个仓库中存放的都是项目模板
   let { data } = await axios.get('https://api.github.com/orgs/gt-cli/repos')
   return data
@@ -10,8 +11,18 @@ const fetchRepoList = async() => {
 }
 // 创建项目
 module.exports = async (projectName, cmd) => {
+  const spinner = ora('fetching repo list');
+  spinner.start(); // 开始loading
   let repos = await fetchRepoList();
-  repos = repos.map(item => item.name)
-  console.log(repos)
+  spinner.succeed(); // 结束loading
+  // 选择模板
+  repos = repos.map((item) => item.name);
+  const { repo } = await Inquirer.prompt({
+    name: 'repo',
+    type: 'list',
+    message: 'please choice repo template to create project',
+    choices: repos, // 选择模式
+  });
   console.log(projectName, cmd);
+  console.log(repo);
 };
