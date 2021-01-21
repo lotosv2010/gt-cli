f51f4b4b20e48690cc3ca5252c44a9f1fd6cbda5
lotosv2010 <lotosv2010@163.com>
2021-01-21 16:43:23 +0800

    feat(axios): add axios and fetch repos list

--- src/action/create.js
+++ src/action/create.js
@@ -1,4 +1,17 @@
+const axios = require('axios');
+
+// 获取仓库列表
+const fetchRepoList = async() => {
+  console.log(111)
+  // 获取当前组织中的所有仓库信息,这个仓库中存放的都是项目模板
+  let { data } = await axios.get('https://api.github.com/orgs/gt-cli/repos')
+  return data
+
+}
 // 创建项目
 module.exports = async (projectName, cmd) => {
+  let repos = await fetchRepoList();
+  repos = repos.map(item => item.name)
+  console.log(repos)
   console.log(projectName, cmd);
 };
